2ea7c403861fce884e4fff6705d8bb4c95b2d55b
Eddie Hernandez <mchh.004@gmail.com>
2019-12-30 19:56:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-	puts programmer_hash[:dennis_ritchie][:languages]
+	programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -83,7 +83,11 @@ def adding_matz
         }
      }
 
-
+programmer_hash[:yukihiro_matsumoto] = {
+   :known_for => "Ruby",
+	 :languages => ["LISP", "C"]
+	 }
+programmer_hash
 end
 
 def changing_alan
